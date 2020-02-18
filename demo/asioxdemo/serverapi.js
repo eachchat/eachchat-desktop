94cdf34b154787c26c1195b36b7449461d46c274
chengfang.ai <chengfang.ai@yunify.com>
2020-02-18 20:49:41 +0800

    完成登陆登出接口。

--- demo/asioxdemo/serverapi.js
+++ demo/asioxdemo/serverapi.js
@@ -1,25 +1,46 @@
 const axios = require('axios');
+var refreshtoken
 
 function initserverapi(protocal, ip, host)
 {
+    console.log("initserverapi")
     var url = protocal + '://' + ip + ':' + host 
     axios.defaults.baseURL = url
+    //axios.defaults.headers = {
+    //    "Accept" : "application/json",
+    //    "Content-type" : "application/json"}
 }
 
-function login(username, password){
-    console.log("登陆")
+function login(username, password)
+{
+    console.log("login")
     axios.post('/api/v1/client/login', {
         'account': username,
         'password': password
     }).then(function (response) {
-        console.log(response.headers);
-        var obj = JSON.parse(response.headers);
-        var accecctoken = obj['access-token']
-        var refreshtoken = obj['refresh-token']
-        if(accecctoken.length > 0)
-            return true
-        else
+        console.log(response)
+        if(response.status != 200)
             return false
+        var tmpheader = response.headers
+        var accesstoken = tmpheader['access-token']
+        refreshtoken = tmpheader['refresh-token']
+         
+        if(accesstoken.length == 0)
+            return false
+        
+        return false
       });
 };
 
+function logout()
+{
+    console.log("logout") 
+    axios.defaults.headers.common['Authorization'] = "Bearer " + refreshtoken;
+    axios.post('/api/v1/client/logout').then(function (response) {
+        console.log(response)
+        if(response.status != 200)
+            return false
+        else
+            return true
+    })
+};
