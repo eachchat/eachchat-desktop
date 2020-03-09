7c829172d8062d089f8e09b8e5bf5d934c72a630
wangxin.ai <wangxin.ai@yunify.com>
2020-03-09 10:47:31 +0800

    界面文件漏了

---
+++ prj/src/renderer/components/chat-content.vue
@@ -0,0 +1,246 @@
+<template>
+    <div class="chat-panel">
+      <div class="chat-list">
+        <div class="list-head">
+          <p class="chat-label">聊天</p>
+          <el-dropdown class="new-chat-dropdown" trigger="click">
+            <span class="new-chat">
+              <i class="el-icon-plus el-icon--right"></i>
+            </span>
+            <el-dropdown-menu class="new-chat-content" slot="dropdown">
+                <el-dropdown-item class="create-new-group" icon="el-icon-connection">
+                  创建群聊
+                </el-dropdown-item>
+                <el-dropdown-item class="create-new-secret" icon="el-icon-lock">
+                  创建密聊
+                </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+        <div class="list-content">
+          <ul class="group-list">
+            <li class="group"
+                v-for="chatGroup in chatGroupList"
+                @click="showChat(chatGroup.name)"
+                >
+              <img class="group-ico" :src="chatGroup.group.groupAvatar">
+              <div class="group-info">
+                <p class="group-name">{{chatGroup.group.groupName}}</p>
+                <p class="group-content">{{getMsgContent(chatGroup.message.content, chatGroup.message.msgContentType)}}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="chat">
+      </div>
+    </div>
+</template>
+
+<script>
+import {ListAllGroup, InitServerAPI, setToken, GetUserinfo} from '../server/serverapi.js'
+export default {
+  components: {
+  },
+  computed: {
+    chatGroupList: {
+      get: function() {
+        return this.$store.state.chatGroup;
+      },
+      set: function(new_list) {
+        this.$store.commit("setChatGroup", new_list)
+      }
+    }
+  },
+  data() {
+    return {
+      //需要展示的用户群组
+    };
+  },
+  methods: {
+    getMsgContent(chatGroupMsgContent, chatGroupMsgType) {
+      //console.log(chatGroupMsgContent)
+      if(chatGroupMsgType === 101)
+      {
+        return chatGroupMsgContent.text
+      }
+      else if(chatGroupMsgType === 102)
+      {
+        return ["图片"]
+      }
+      else if(chatGroupMsgType === 103)
+      {
+        return ["文件"]
+      }
+      else if(chatGroupMsgType === 104)
+      {
+        if(chatGroupMsgContent.type === "invitation")
+        {
+          var invitee = chatGroupMsgContent.userInfos.userName
+          var inviter = chatGroupMsgContent.userName
+          return inviter + " 邀请 " + invitee + " 加入群聊"
+        }
+        else if(chatGroupMsgContent.type === "notice")
+        {
+          var owner = chatGroupMsgContent.userName
+          return owner + " 发布群公告"
+        }
+        else if(chatGroupMsgContent.type === "updateGroupName")
+        {
+          var owner = chatGroupMsgContent.userName
+          var distName = chatGroupMsgContent.text
+          return owner + " 修改群名称为 " + distName
+        }
+        else if(chatGroupMsgContent.type === "deleteGroupUser")
+        {
+          var owner = chatGroupMsgContent.userName
+          var bybyer = chatGroupMsgContent.userInfos.userName
+          return owner + " 将 " + bybyer + " 移出了群聊"
+        }
+        else
+        {
+          return "您收到一条短消息"
+        }
+      }
+      else if(chatGroupMsgType === 105)
+      {
+        return ["语音"]
+      }
+      else if(chatGroupMsgType === 106)
+      {
+        return ["聊天记录"]
+      }
+      return "123"
+    }
+  },
+  created: function() {
+    console.log("Created")
+    setToken(this.$store.state.accesstoken, this.$store.state.refreshtoken)
+    InitServerAPI('http', '139.198.15.253')
+    ListAllGroup()
+        .then((response) => {
+            //console.log(response)
+            var ret_data = response.data
+            var ret_list = ret_data.results
+            this.$store.commit("setChatGroup", ret_list)
+        })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .chat-panel {
+    width: 890px;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+  .chat {
+    width:610px;
+    flex: 1;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+
+  .new-chat-dropdown {
+    float: right;
+    height: 60px;
+    width: 60px;
+    border: 0px;
+  }
+
+  .new-chat {
+    line-height: 60px;
+    border: 0px;
+    background-color: rgb(242, 242, 246);
+  }
+
+  .chat-label {
+    float: left;
+    overflow: hidden;
+    height: 60px;
+    width:68px;
+    margin:0;
+    line-height: 60px;
+    text-align: center;
+  }
+
+  .chat-list {
+    height: 100%;
+    width: 280px;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid rgb(242, 242, 246);
+  }
+
+  .list-head {
+    display: block;
+    width: 100%;
+    height: 60px;
+    background-color: rgb(242, 242, 246);
+  }
+
+  .list-content {
+    height: 100%;
+    overflow-y: scroll;
+    
+  }
+
+  .group-list {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+  .group {
+    height: 50px;
+    border: 0.3px solid rgba(242, 242, 246, 10);
+  }
+
+  .group-ico {
+    width: 30px;
+    height: 30px;
+    display: inline-block;
+    margin-left: 10px;
+    margin-top: 10px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+  
+  .group-info {
+    display: inline-block;
+    vertical-align: top;
+    height: 100%;
+    width: calc(100% - 60px);
+  }
+
+  .group-name {
+    width: calc(100% - 20px);
+    height: 18px;
+    font-size: 14px;
+    color: rgb(57, 57, 57);
+    overflow: hidden;
+    margin-left: 10px;
+    margin-top: 10px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .group-content {
+    font-size: 13px;
+    color: rgb(170, 179, 178);
+    overflow: hidden;
+    margin-left: 10px;
+    margin-top: 5px;
+    margin-right: 10px;
+    margin-bottom: 0px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+</style>
