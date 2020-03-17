631c626d11ad279bc3be7f3a0c923cb6122b3a46
chengfang.ai <chengfang.ai@yunify.com>
2020-03-17 16:21:56 +0800

    删除多余的文件夹。

--- prj/src-before-git-update/renderer/server/Utils.js
+++
@@ -1,9 +0,0 @@
-// Common Interface
-
-//https://blog.csdn.net/qq_37568049/article/details/80736305
-export function generalGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
