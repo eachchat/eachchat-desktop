594f331f4040073f7131b332a7eddf2990097946
Jakit <jakit_mail@163.com>
2020-03-30 13:53:36 +0800

    Redesign service

---
+++ prj/src/packages/data/index.js
@@ -0,0 +1,15 @@
+/**
+ * Data
+ * @author Jakit
+ * @date 2020/03/30
+ */
+
+import * as services from './services.js'
+
+const data = {
+  services: services
+}
+
+export {
+  data
+}
--- prj/src/packages/data/services.js
+++ prj/src/packages/data/services.js
@@ -2,89 +2,118 @@
 import { models } from './models.js';
 import { APITransaction } from './transaction.js';
 
-class CommonService extends APITransaction {
-  constructor(hostname) {
-    super(hostname);
+const commonConfig = {
+  hostname: undefined,
+  apiPort: undefined,
+  username: undefined,
+  password: undefined
+}; // config info
+
+const commonModels = {
+  login: models.Login
+}; // model in here
+
+const common = {
+  config: commonConfig,
+
+  data: commonModels,
+
+  api: undefined,
+
+  init(config) {
+    if ("hostname" in config) {
+      this.config.hostname = config.hostname;
+    }
+
+    if ("apiPort" in config) {
+      this.config.apiPort = config.apiPort;
+    }
+
+    if ("username" in config) {
+      this.config.username = config.username;
+    }
+
+    if ("password" in config) {
+      this.config.password = config.password;
+    }
+
+    this.api = new APITransaction(this.config.hostname, this.config.hostname);
   }
 
-  async login(username, password) {
-    let result = await super.login(username, password);
+  get login() {
+    return (async (api, config, model) => {
+      let result = await super.login(username, password);
 
-    var loginValues = {
-      id: undefined,
-      access_token: undefined,
-      refresh_token: undefined,
-      account: username,
-      password: password
-    };
+      var loginValues = {
+        id: undefined,
+        access_token: undefined,
+        refresh_token: undefined,
+        account: username,
+        password: password
+      };
 
-    var userValues = {
-      id: undefined,#
-      account: username,
-      name: undefined,#
-      pinyin: undefined,#
-      nick_name: undefined,#
-      avatar: undefined,#
-      avatar_minimal: undefined,#
-      role_id: undefined,#
-      role_name: undefined,#
-      language: undefined,
-      locale: undefined,
-      timezone: undefined,
-      is_active: undefined,
-      bio: undefined
-    };
+      var userValues = {
+        id: undefined,#
+        account: username,
+        name: undefined,#
+        pinyin: undefined,#
+        nick_name: undefined,#
+        avatar: undefined,#
+        avatar_minimal: undefined,#
+        role_id: undefined,#
+        role_name: undefined,#
+        language: undefined,
+        locale: undefined,
+        timezone: undefined,
+        is_active: undefined,
+        bio: undefined
+      };
 
-    if (!result.ok || !result.success) {
-      return undefined;
-    }
-
-    if (!("obj" in result.data)) {
-      return undefined;
-    }
-
-    var headersHave = {
-      "access-token": "access_token",
-      "refresh-token": "refresh_token"
-    };
-
-    var userObjectHave = {
-      "aId",
-      "id",
-      "userName",
-      "displayName",
-      "displayNamePy",
-      "nickName",
-      "avatarOUrl",
-      "avatarTUrl",
-      "title",
-      "preferredLanguage",
-      "locale",
-      "timezone",
-      "active",
-      "statusDescription"
-    };
-
-    for (var i = 0; i < headersHave.length; i++) {
-      if (headersHave[i] in result.data) {
-
+      if (!result.ok || !result.success) {
+        return undefined;
       }
-    }
+
+      if (!("obj" in result.data)) {
+        return undefined;
+      }
+
+      var headersHave = {
+        "access-token": "access_token",
+        "refresh-token": "refresh_token"
+      };
+
+      var userObjectHave = {
+        "aId": "id",
+        "displayName": "name",
+        "displayNamePy": "pinyin",
+        "nickName": "nick_name",
+        "avatarOUrl": "avatar",
+        "avatarTUrl": "avatar_minimal",
+        "title": "role_name",
+        "preferredLanguage": "language",
+        "locale": "locale",
+        "timezone": "timezone",
+        "active": "is_active",
+        "statusDescription": "bio"
+      };
+
+      for (var key in headersHave) {
+        if (key in result.headers) {
+          loginValues[headersHave[key]] = result.headers[key];
+        }
+      }
+
+      for (var key in userObjectHave) {
+        if (key in result.data.obj) {
+          userValues[userObjectHave[key]] = result.data.obj[key];
+        }
+      }
+    })(this.api, this.config, this.data.login);
   }
 
-  // async logout() {
-  //   return this.apiTransaction.Logout().then(function(response){
-  //       console.log(response)
-  //   })
-  // }
+  get logout() {}
+};
 
-  // async getUserinfo(filters_value, perPage_value, sortOrder_value, sequenceId_value) {
-  //   return this.apiTransaction.getUserinfo(filters_value, perPage_value, sortOrder_value, sequenceId_value).then(function(response){
-  //       console.log(response)
-  //   })
-  // }
-}
-
-export {
-  CommonService
+export default {
+  common
 }
