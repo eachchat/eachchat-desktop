c296f3b2c5e1b4750136b76032f6a09f854c40c6
chengfang.ai <chengfang.ai@yunify.com>
2020-04-05 19:46:09 +0800

    删除测试代码。

--- prj/src/renderer/components/name-login.vue
+++ prj/src/renderer/components/name-login.vue
@@ -45,9 +45,6 @@ export default {
         clickUser () {
             location.reload()
         },
-        callback(msg){
-            console.log('vue-log:' + msg)
-        },
         login: async function() {
             let response = await this.serverapi.Login(this.username, this.password)
             console.log(response)
