da117f571cb85230e99571380fe5a6c25e4a6ffd
chengfang.ai <chengfang.ai@yunify.com>
2020-06-18 21:46:09 +0800

    添加测试代码。

--- prj/test/e2e/specs/service.spec.js
+++ prj/test/e2e/specs/service.spec.js
@@ -5,7 +5,7 @@ import {services} from "../../../src/packages/data/index.js"
 describe('ServiceTest', function () {
 
   it("LoginTestCase", async function(){
-    this.timeout(5000)
+    this.timeout(50000);
     let config = {
       hostname: "139.198.15.253",
       apiPort: 8888,
@@ -15,13 +15,26 @@ describe('ServiceTest', function () {
     services.common.init(config);
     console.log("begin login");
     let loginResult = await services.common.login();
-    expect(loginResult).to.equal(true)
+    expect(loginResult).to.equal(true);
   })
 
-  it("AllUserinfoTestCase", async ()=>{
-    this.timeout(30000);
+  it("AllUserinfoTestCase", async function (){
+    return;
+    this.timeout(300000);
     let result = await services.common.AllUserinfo();
-    expect(result).to.be(true);
+    console.log(result)
+    expect(result).to.equal(true);
   })
-  
+
+  it("AllDepartmentInfoTestCase", async function(){
+    this.timeout(30000);
+    let result = await services.common.AllDepartmentInfo();
+    expect(result).to.equal(true);
+  })
+
+  it("listAllGroupTestCase", async function(){
+    this.timeout(500000);
+    let result = await services.common.listAllGroup();
+    expect(result).to.equal(true);
+  })
 })
