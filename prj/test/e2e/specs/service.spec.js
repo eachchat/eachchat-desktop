36b25c7092cd7ac68ca6785a01f81f0a7459bbe5
chengfang.ai <chengfang.ai@yunify.com>
2020-07-18 10:28:34 +0800

    添加输入组织架构的接口。

--- prj/test/e2e/specs/service.spec.js
+++ prj/test/e2e/specs/service.spec.js
@@ -9,20 +9,20 @@ describe('ServiceTest', function () {
   let selfUser;
   let resSendMessage;
   let sendNewMessageResponse;
+
   it("LoginTestCase", async function(){
     this.timeout(50000);
+    
     let config = {
-      hostname: "139.198.15.253",
-      apiPort: 8888,
       username: "chengfang.ai@yunify.com",
       password: "12345678"
     };
-    services.common.init(config);
-    console.log("begin login");
-    let loginResult = await services.common.login();
+    
+    await services.common.gmsConfiguration("dev.eachchat.net");
+    await services.common.init();
+    let loginResult = await services.common.login(config);
     expect(loginResult).to.equal(true);
     this.selfUser = await services.common.GetSelfUserModel();
-
   })
 
   it("AllUserinfoTestCase", async function (){
@@ -48,30 +48,35 @@ describe('ServiceTest', function () {
   })
 
   it("ListMessageCollectionTestCase", async function(){
+    return;
     let netRes = await services.common.ListMessageCollections();
     let dbRes = await Collection.FindCollectionByType(101);
     expect(netRes.length).to.equal(dbRes.length);
   })
 
   it("ListPictrueCollectionTestCase", async function(){
+    return;
     let netRes = await services.common.ListPictureCollections();
     let dbRes = await Collection.FindCollectionByType(102);
     expect(netRes.length).to.equal(dbRes.length);
   })
 
   it("ListFileCollectionsTestCase", async function(){
+    return;
     let netRes = await services.common.ListFileCollections();
     let dbRes = await Collection.FindCollectionByType(103);
     expect(netRes.length).to.equal(dbRes.length);
   })
 
   it("ListGroupCollectionsTestCase", async function(){
+    return;
     let netRes = await services.common.ListGroupCollections();
     let dbRes = await Collection.FindCollectionByType(104);
     expect(netRes.length).to.equal(dbRes.length);
   })
 
   it("ListTopicCollectionsTestCase", async function(){
+    return;
     let netRes = await services.common.ListTopicCollections();
     let dbRes = await Collection.FindCollectionByType(106);
     expect(netRes.length).to.equal(dbRes.length);
@@ -100,7 +105,6 @@ describe('ServiceTest', function () {
   })
 
   it("CreateGroupTestCase", async function(){
-    return;
     //this.resCreateGroup.group_id = "5ec4fda7cc101002783bcb77";
     this.resCreateGroup = await services.common.CreateGroup("test group", ["eb69c200e11801906322203ad59ff885", "8a93c2debb75bc4204f03c88c9dc11b1", "381c86d8e2aca3e992224d5b3061c442", "e3b18ba88b23296d6bb28fb23baab034", "516fec24899afddbe0b9f51378da6372",
     "509c86d45a7e6a1afe7e931425cf428f"]);
@@ -109,23 +113,26 @@ describe('ServiceTest', function () {
   })
 
   it("AddGroupUserTestCase", async function(){
-    return;
-    let res = await services.common.AddGroupUsers('5f06c25bcc10105b06aff96b', ["157668e1fc2c0a054af0244116a09e36"]);
+    //let res = await services.common.AddGroupUsers('5f06c25bcc10105b06aff96b', ["157668e1fc2c0a054af0244116a09e36"]);
+    let res = await services.common.AddGroupUsers(this.resCreateGroup.group_id, ["157668e1fc2c0a054af0244116a09e36"]);
+
+    
     expect(res).to.equal(true);
   })
 
   it("DeleteGroupUserTestCase", async function(){
-    return;
-    let res = await services.common.DeleteGroupUsers('5f06c25bcc10105b06aff96b',["157668e1fc2c0a054af0244116a09e36"]);
+    //let res = await services.common.DeleteGroupUsers("5f06c25bcc10105b06aff96b",["157668e1fc2c0a054af0244116a09e36"]);
+    let res = await services.common.DeleteGroupUsers(this.resCreateGroup.group_id,["157668e1fc2c0a054af0244116a09e36"]);
     expect(res).to.equal(true);
   })
 
   it("DeleteGrupTestCase", async function(){
-    //let res = await services.common.DeleteGroup(this.resCreateGroup.group_id);
-    //expect(res).to.equal(true);
+    let res = await services.common.DeleteGroup(this.resCreateGroup.group_id);
+    expect(res).to.equal(true);
   })
 
   it("sendNewMessageTestCase", async function(){
+    return;
     let msgID = generalGuid();
     let curTimeSeconds = new Date().getTime();
     let response = await services.common.sendNewMessage(msgID, 101, "25d4cb78d54840dfa70df0dfa847c024", "5e815b92cc101019c46c2eea", "eb69c200e11801906322203ad59ff885", curTimeSeconds, {text:"test message"});
@@ -149,6 +156,7 @@ describe('ServiceTest', function () {
 
   //collectionmessage
   it("CollectMessageTestCase", async function(){
+    return;
     let array = [];
     array.push(this.sendNewMessageResponse.time_line_id);
     let result = await services.common.CollectMessage(array);
@@ -156,6 +164,7 @@ describe('ServiceTest', function () {
   })
 
   it("CollectGroupTestCase", async function(){
+    return;
       let result = await services.common.CollectGroup("5e9ea897cc10101597c8959f", this.selfUser.id);
       expect(result).to.equal(true);
   })
@@ -178,6 +187,11 @@ describe('ServiceTest', function () {
 
     //expect(result).to.not.equal(false);
   })
+  
+  it("GetRecentDeviceTestCase", async function(){
+//response = await services.common.GetRecentDevice();
+
+  })
 
 
 })
@@ -199,7 +213,6 @@ describe('ServiceTest', function () {
 //for(let index in response){
 //console.log(response[index].sequenceValue)
 //}
-//await services.common.InitServiceData();
 //await services.common.ListGroupFiles("5eec82a1cc10105e30d318a5", "0");
 //response = await services.common.historyMessage("5e815b92cc101019c46c2eea", "449677749993881600", 20);
 //for(let index in response){
