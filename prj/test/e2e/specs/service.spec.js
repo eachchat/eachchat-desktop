d16f18c7dabddbcc8f037e91df8ab257b349561f
chengfang.ai <chengfang.ai@yunify.com>
2020-08-11 17:37:20 +0800

    修改单元测试错误。

--- prj/test/e2e/specs/service.spec.js
+++ prj/test/e2e/specs/service.spec.js
@@ -198,7 +198,7 @@ describe('ServiceTest', function () {
     //expect(result).to.not.equal(false);
   })
 
-  it("UpdateUserAvatarTestCase", async function{
+  it("UpdateUserAvatarTestCase", async function(){
     //let tmp = await services.common.UpdateUserAvatar("C:\\Users\\chengfang\\Desktop\\laofuzi.png");
     //console.log(tmp);
     
