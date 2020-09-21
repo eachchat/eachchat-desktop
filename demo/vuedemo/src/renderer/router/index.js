35a5bb11d74a7be17c3b28655fd969b81ab31644
chengfang.ai <chengfang.ai@yunify.com>
2020-09-21 09:53:26 +0800

    提交vuedemo。

--- demo/vuedemo/src/renderer/router/index.js
+++ demo/vuedemo/src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'matrix-page',
+      component: require('@/components/Matrix').default
     },
     {
       path: '*',
