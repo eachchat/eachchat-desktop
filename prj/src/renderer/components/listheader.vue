7a9be2e49b0664dd630074e16e53a57bfd0c792c
vincent <vincent@vincentdeMacBook-Pro.local>
2020-12-04 00:22:39 +0800

    恢复已有房间进入的回调

--- prj/src/renderer/components/listheader.vue
+++ prj/src/renderer/components/listheader.vue
@@ -106,7 +106,7 @@ export default {
         mxDmDlg: function(close) {
             console.log('???', close)
             if (close) {
-                // if (close.data) this.$emit(close.handler, close.data);
+                if (close.data) this.$emit(close.handler, close.data);
                 this.showCreateNewChat = false;
                 return this.mxDmDlgOpen = false;
             }
