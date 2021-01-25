4844ba3e78bc5f13254004cb76e9c2e6033c0349
chengfang.ai <chengfang.ai@yunify.com>
2021-01-25 16:04:48 +0800

    搜索用户不匹配职位信息。

--- prj/src/packages/data/sqliteutil.js
+++ prj/src/packages/data/sqliteutil.js
@@ -424,7 +424,6 @@ const UserInfo = {
         let infos = await(await models.UserInfo).find({
             user_display_name:  "%"+key,
             _user_name:          "%"+key,
-            _user_title:         "%"+key,
             _display_name_py:    "%"+key,
             $size: 20
         })
