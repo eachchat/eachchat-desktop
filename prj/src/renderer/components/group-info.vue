4557861530f44711897f9096cb60a8acf8a0af41
vincent <vincent@vincentdeMacBook-Pro.local>
2021-01-27 18:04:11 +0800

    右侧栏主头catch onerror

--- prj/src/renderer/components/group-info.vue
+++ prj/src/renderer/components/group-info.vue
@@ -6,7 +6,7 @@
         <div class="groupInfo-view">
             <div v-if="!isDm" class="groupInfoImageDiv" style="margin-right: 8px;">
                 <input style="display:none;" id="mxavai" @change="_onAvatarChanged" type="file" accept="image/*">
-                <img id="groupInfoImageId" class="groupInfoImage" :src="mxAvatar">
+                <img id="groupInfoImageId" class="groupInfoImage" :src="mxAvatar" onerror = "this.src = './static/Img/User/group-40px@2x.png'">
                 <img 
                     id="groupInfoImageChangeId" 
                     class="groupInfoImageChange" 
@@ -18,7 +18,9 @@
                 <img 
                     id="groupInfoImageId" 
                     class="groupInfoImage" 
-                    :src="mxAvatar">
+                    :src="mxAvatar"
+                    onerror = "this.src = './static/Img/User/user-40px@2x.png'"
+                >
             </div>
             <div class="groupInfoNoticeAndName" v-if="!isDm">
                 <div class="groupInfoName">
