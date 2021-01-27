b515731d8a9776a4702f373c61d310bd4aeeb302
vincent <vincent@vincentdeMacBook-Pro.local>
2021-01-27 18:07:20 +0800

    update

--- prj/src/renderer/components/group-info.vue
+++ prj/src/renderer/components/group-info.vue
@@ -6,7 +6,7 @@
         <div class="groupInfo-view">
             <div v-if="!isDm" class="groupInfoImageDiv" style="margin-right: 8px;">
                 <input style="display:none;" id="mxavai" @change="_onAvatarChanged" type="file" accept="image/*">
-                <img id="groupInfoImageId" class="groupInfoImage" :src="mxAvatar" onerror = "this.src = './static/Img/User/group-40px@2x.png'">
+                <img id="groupInfoImageId" class="groupInfoImage" :src="mxAvatar" onerror="this.src = './static/Img/User/group-40px@2x.png'">
                 <img 
                     id="groupInfoImageChangeId" 
                     class="groupInfoImageChange" 
@@ -19,7 +19,7 @@
                     id="groupInfoImageId" 
                     class="groupInfoImage" 
                     :src="mxAvatar"
-                    onerror = "this.src = './static/Img/User/user-40px@2x.png'"
+                    onerror="this.src = './static/Img/User/user-40px@2x.png'"
                 >
             </div>
             <div class="groupInfoNoticeAndName" v-if="!isDm">
@@ -166,7 +166,7 @@
                     </div> -->
                     <!-- <img class="groupMemberClickOut" :id="getDeleteIdThroughMemberUid(item.user_id)" src="../../../static/Img/Chat/delete-20px@2x.png" @click="deleteMember(item)" v-show="notOwner(item)"> -->
                     <div class="memberItemLeft">
-                        <img onerror = "this.src = './static/Img/User/user-40px@2x.png'" :src="item.mxAvatar" class="memberItemAvatar" :id="getIdThroughMemberUid(item.userId)" @click="showUserInfoTip($event, item)"> <!--todo 头像需要更替-->
+                        <img onerror="this.src = './static/Img/User/user-40px@2x.png'" :src="item.mxAvatar" class="memberItemAvatar" :id="getIdThroughMemberUid(item.userId)" @click="showUserInfoTip($event, item)"> <!--todo 头像需要更替-->
                         <div class="memberItemContent"  @click="showUserInfoTip($event, item)">
                             <div class="memberItemName">
                                 <span>{{item.dspName}}</span>
--- prj/src/renderer/components/mxSquareDlg.vue
+++ prj/src/renderer/components/mxSquareDlg.vue
@@ -14,7 +14,7 @@
                 </div>
                 <div class="room-list">
                     <div v-for="item in publicRooms" :key="item.room_id" class="room-item">
-                        <img class="room-img" :src="item.distUrl" onerror = "this.src = './static/Img/User/group-40px@2x.png'"/>
+                        <img class="room-img" :src="item.distUrl" onerror="this.src = './static/Img/User/group-40px@2x.png'"/>
                         <div class="room-xie">
                             <div class="room-xie1" v-if="item.name" v-html="searchKeyHightLight(item.name)">{{item.name + '(' + item.num_joined_members + ')'}}</div>
                             <div style="width:200px; text-overflow:ellipsis; overflow:hidden; white-space:nowrap; display:block;" class="room-xie2" v-if="item.topic">{{item.topic}}</div>
