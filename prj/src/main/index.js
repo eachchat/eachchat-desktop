91613c1e0a127d5201ee1505e3d77bdfe762095a
chengfang.ai <chengfang.ai@yunify.com>
2021-01-28 15:31:50 +0800

    默认关闭开发模式。

--- prj/src/main/index.js
+++ prj/src/main/index.js
@@ -1103,6 +1103,7 @@ function openDevToolsInDevelopment(mainWindow) {
 
   // Open dev tools initially when in development mode
   if (process.env.NODE_ENV === "development") {
+    return;
     mainWindow.webContents.on("did-frame-finish-load", () => {
     mainWindow.webContents.once("devtools-opened", () => {
     mainWindow.focus();
