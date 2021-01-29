e47d790bf5ebe77fc4413b0499a28d3cbccb7d33
benjamin <qhjinan@gmail.com>
2021-01-29 17:03:39 +0800

    :bug: 修复手机端更换头像，本地头像未更换

--- prj/src/renderer/components/chat.vue
+++ prj/src/renderer/components/chat.vue
@@ -2948,27 +2948,6 @@ export default {
                 }
             }
         },
-        UpdateUserAvater(ev){
-            if(ev.getType() === 'm.room.member' && ev.getSender() === this.userID){
-                if(ev.event && ev.event.content){
-                    let content = ev.event.content;
-                    if(content.is_direct)
-                        return;
-                    if(content.membership != 'join')
-                        return;
-                    let url = content.avatar_url;
-                    var avaterUrl = global.mxMatrixClientPeg.matrixClient.mxcUrlToHttp(url, null, null);
-                    var elementImg = document.getElementById("userHead");
-                    if(elementImg){
-                        if(avaterUrl == ''){
-                            elementImg.setAttribute('src', '../../../static/Img/User/user-40px@2x.png');
-                        }
-                        else
-                            elementImg.setAttribute('src', avaterUrl);
-                    }
-                }
-            }
-        },
 
         IsBottom: function(){
             let uldiv = document.getElementById("message-show-list");
