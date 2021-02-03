036e0f09c0f7cc7562f4ab4da632017a5d3aa397
wangxin.ai <wangxin.ai@yunify.com>
2021-02-03 14:38:45 +0800

    matrix的timelineset的filter，初始化过慢，现在替换回本地自己的filter

--- prj/src/renderer/components/chat.vue
+++ prj/src/renderer/components/chat.vue
@@ -3068,11 +3068,12 @@ export default {
                 let index = 0;
                 msgList = [];
                 tmpList.forEach(item => {
-                    // if(msgFileter(item) && item.event.content){
-                    if(!this.isDeleted(item)){
-                        msgList.push(item);
-                        index++;
-                    } 
+                    if(msgFileter(item) && item.event.content){
+                        if(!this.isDeleted(item)){
+                            msgList.push(item);
+                            index++;
+                        } 
+                    }
                 })
                 if(index > num) break;
             }
@@ -3426,17 +3427,29 @@ export default {
             return timelineSet;
         },
         async toGetShowMessage() {
-            this.timeLineSet = await this.updateTimelineSet(this.curChat);
+            // this.timeLineSet = await this.updateTimelineSet(this.curChat);
+            this.timeLineSet = this.curChat.getUnfilteredTimelineSet();
             this._timelineWindow = new Matrix.TimelineWindow(
                 global.mxMatrixClientPeg.matrixClient, 
                 this.timeLineSet,
                 {windowLimit:Number.MAX_VALUE},
             )
             await this._timelineWindow.load(undefined, 20);
-            var fileListInfo = this._timelineWindow.getEvents();
+            var fileListInfo = [];
+            var originalFileListInfo = this._timelineWindow.getEvents();
+            originalFileListInfo.forEach(item => {
+                if(this.messageFilter(item) && item.event.content){
+                    fileListInfo.push(item);
+                } 
+            })
             while(fileListInfo.length < 10 && this._timelineWindow.canPaginate('b')) {
                 await this._timelineWindow.paginate("b", 20);
-                fileListInfo = await this._timelineWindow.getEvents();
+                var fileListInfoTmp = await this._timelineWindow.getEvents();
+                fileListInfoTmp.forEach(item => {
+                    if(this.messageFilter(item) && item.event.content){
+                        fileListInfo.push(item);
+                    }
+                })
             }
             return fileListInfo;
         },
