5b2f24a1b6a8094abaa0d1a36a3f58518529ccf4
wangxin.ai <wangxin.ai@yunify.com>
2021-03-29 16:56:13 +0800

    表情的正则匹配bug，引起有的表情无法正常放大

--- prj/src/renderer/components/emoji.vue
+++ prj/src/renderer/components/emoji.vue
@@ -39,8 +39,17 @@ export default {
         var ranges = [
           '\ud83c[\udf00-\udfff]', 
           '\ud83d[\udc00-\ude4f]', 
-          '\ud83d[\ude80-\udeff]'
+          '\ud83d[\ude80-\udeff]',
+          '\ud83d[\udc00-\udec5]',
+          '\ud83c[\udd70-\udf4e]',
+          '\ud83d[\udc00-\udec5]|\ud83c[\udd70-\udf4e]', 
+          '\ud83c[\udf00-\udfff]|\ud83d[\udc00-\ude4f]', 
+          '\ud83c[\udc00-\udfff]|\ud83d[\udc00-\udfff]|[\u2000-\u2fff]',
+          '(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])',
+          /([\u{1F300}-\u{1F5FF}][\u{2000}-\u{206F}][\u{2700}-\u{27BF}]|([\u{1F900}-\u{1F9FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F600}-\u{1F64F}])[\u{2000}-\u{206F}][\u{2600}-\u{26FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F100}-\u{1F1FF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F200}-\u{1F2FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1F000}-\u{1F02F}]|[\u{FE00}-\u{FE0F}]|[\u{1F0A0}-\u{1F0FF}]|[\u{0000}-\u{007F}][\u{20D0}-\u{20FF}]|[\u{0000}-\u{007F}][\u{FE00}-\u{FE0F}][\u{20D0}-\u{20FF}])$/u,
+          /[\u{2100}-\u{214F}]|[\u{2000}-\u{206F}]|[\u{2460}-\u{24FF}]|[\u{0080}-\u{00FF}]|[\u{2700}-\u{27BF}]|[\u{2600}-\u{26FF}]|[\u{2B00}-\u{2BFF}]|[\u{2300}-\u{23FF}]|[\u{25A0}-\u{25FF}]|[\u{3000}-\u{303F}]|[\u{3200}-\u{32FF}]|[\u{2900}-\u{297F}]|[\u{2190}-\u{21FF}]/u
           ];
+
         let codedText = this.text.replace(new RegExp(ranges.join('|'), 'g'), function(emoji){
             return `%-special-code-%${emoji}%-special-code-%`
           });
