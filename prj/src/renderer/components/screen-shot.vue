125d997bb5eee523469dbbf9cc29aff02538a9d6
chengfang.ai <chengfang.ai@yunify.com>
2021-05-07 19:41:36 +0800

    完成截屏并显示到界面上，后面需要创建一个全屏渲染进程，在新的渲染进程上显示剪切后的图像。

--- prj/src/renderer/components/screen-shot.vue
+++ prj/src/renderer/components/screen-shot.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-
+    <div class = "ScreenShotLayers">
+        <canvas id="desktop-canvas" width="250" height="300" style="border:1px solid #d3d3d3;"></canvas>
     </div>
 </template>
 
@@ -15,17 +15,36 @@ export default {
     name:'screenShotDlg',
     data(){
         return{
-
+            screenSize:{}
         }
     },
 
     methods:{
         determineScreenShotSize () {
             const screenSize = screen.getPrimaryDisplay().workAreaSize
-            const maxDimension = Math.max(screenSize.width, screenSize.height)
+            const maxDimension = Math.max(screenSize.width, screenSize.height);
+            this.screenSize.width = screenSize.width;
+            this.screenSize.height = screenSize.height;
+
             return {
                 width: maxDimension * window.devicePixelRatio,
                 height: maxDimension * window.devicePixelRatio
+            }
+        },
+
+        drowCanvas(imgData){
+            let canvas = document.getElementById('desktop-canvas')
+            canvas.width = this.screenSize.width
+            canvas.height = this.screenSize.height
+            canvas.style.width = this.screenSize.width+'px'
+            canvas.style.height = this.screenSize.height+'px'
+            const ctx = canvas.getContext('2d')
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            let img = new Image();
+            img.src = imgData;
+            img.onload = () =>{
+                ctx.drawImage(img, 0, 0);
             }
         }
     },
@@ -37,13 +56,8 @@ export default {
         desktopCapturer.getSources(options).then((sources) => {
             sources.forEach((source) => {
                 if (source.name === 'Entire Screen' || source.name === 'Screen 1') {
-                    const screenshotPath = path.join(os.tmpdir(), 'screenshot.png')
-                    let tmpPhoto = source.thumbnail.toPNG();
-                    console.log("getfile")
-                    //fs.writeFile(screenshotPath, source.thumbnail.toPNG(), (error) => {
-                    //if (error) return console.log(error)
-                    //shell.openExternal(`file://${screenshotPath}`)
-                    //})
+                    let tmpbmp = source.thumbnail.toBitmap();
+                    this.drowCanvas(tmpbmp);
                 }
             })
         })
@@ -55,5 +69,13 @@ export default {
 }
 </script>
 <style>
-
+    .ScreenShotLayers {
+        height: 100%;
+        width: 100%;
+        position: fixed;
+        top:0px;
+        left:0px;
+        background: rgba(0, 0, 0, 0.6);
+        z-index:3;
+    }
 </style>
