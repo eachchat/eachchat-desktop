d2b86d50b978021ce6245bf078c81adcc59a9238
chengfang.ai <chengfang.ai@yunify.com>
2021-06-18 11:18:40 +0800

    hangup needn't call "ipcRenderer.emit("close");"

--- prj/src/packages/data/mxVoIP.js
+++ prj/src/packages/data/mxVoIP.js
@@ -285,7 +285,6 @@ class mxVoIP{
             global.mxMatrixClientPeg.getCall(room_id).hangup();
             global.mxMatrixClientPeg.removeCall(room_id);
         }
-        ipcRenderer.emit("close");
         updateTrayNotice();
     }
 
