937f525bab41f3e6da12fdb73af43221e982feb6
chengfang.ai <chengfang.ai@yunify.com>
2021-06-29 14:29:56 +0800

    fix don't show quate message in old version.

--- prj/src/utils/commonFuncs.js
+++ prj/src/utils/commonFuncs.js
@@ -22,6 +22,7 @@ export function openBaseMenu() {
 
 export function getImgUrlByEvent (event) {
     var distUrl = (event.content.info && event.content.info.thumbnail_url && event.content.info.thumbnail_url.length != 0) ? event.content.info.thumbnail_url : event.content.url;
+    if(!distUrl) return;
     if(!distUrl.startsWith('blob:')) {
         let iconPath = global.mxMatrixClientPeg.matrixClient.mxcUrlToHttp(distUrl);
         return iconPath;
