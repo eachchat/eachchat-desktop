8ff9854320ca8d83abe199ab8f0bafd800d655c6
wx870601 <wangxin@workly.ai>
2021-07-09 14:11:37 +0800

    add new contextmenu global plugin, current not used

---
+++ prj/src/plugins/ContextMenu/contextMenu.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="contextMenu" id="contextMenuId">
+        <ul class="menuList">
+            <li class="menuItem"
+                v-for="menuItem in menuItemList"
+                @click="callback(menuItem)">
+                {{menuItem.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContextMenu',
+    props: {
+    },
+    data () {
+        return {
+            menuItemList: undefined,
+            distItem: undefined,
+        }
+    },
+    methods: {
+        callback: function(item) {
+            console.log("====item ", item);
+            console.log("====distItem ", this.distItem);
+            item.func(this.distItem);
+        }
+    },
+    components: {
+    },
+    created: async function () {
+        console.log(this.menuItemList)
+    },
+    mounted: function() {
+        console.log(this.menuItemList)
+    },
+    watch: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .contextMenu {
+        position: absolute;
+        height: 48px;
+        background-color: rgba(255, 255, 255, 1);
+        box-shadow: 0px 0px 12px 0px rgba(103, 103, 103, 0.14);
+        border-radius: 4px;
+        border: 1px solid rgba(103, 103, 103, 0.14);
+    }
+
+    .menuItem {
+        padding: 7px 27px 7px 27px;
+        height: 24px;
+    }
+</style>
