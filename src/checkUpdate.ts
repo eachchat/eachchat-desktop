fe7377ad09f209d58fc18491aa815afedaeafb6c
ZhaoBingyu <13843076612@163.com>
2023-03-23 18:05:26 +0800

    feat: update

--- src/checkUpdate.ts
+++ src/checkUpdate.ts
@@ -14,34 +14,50 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { app, dialog } from "electron";
+import { dialog } from "electron";
 import { autoUpdater } from "electron-updater";
+
+
 
 export const checkUpdate = (checkUpdateUrl: string)=>{
   if(!checkUpdateUrl) return;
+
   if(process.platform == 'darwin'){  
     //我们使用koa-static将静态目录设置成了static文件夹，
-    //所以访问http://127.0.0.1:9005/darwin，就相当于访问了static/darwin文件夹，win32同理
     autoUpdater.setFeedURL(`${checkUpdateUrl}/darwin`)  //设置要检测更新的路径
   }else{
     autoUpdater.setFeedURL(`${checkUpdateUrl}/win32`)
   }
-  
-  //检测更新
-  autoUpdater.checkForUpdates()
-  
-  //监听'error'事件
-  autoUpdater.on('error', (err) => {
-    console.log(err)
-  })
-  
-  //监听'update-available'事件，发现有新版本时触发
-  autoUpdater.on('update-available', () => {
-    console.log('found new version')
-  })
-  
+
   //默认会自动下载新版本，如果不想自动下载，设置autoUpdater.autoDownload = false
+
+  //检测更新
+  autoUpdater.checkForUpdates();
+
+  // 监听 'checking-for-update' 事件
+  autoUpdater.on('checking-for-update', () => {
+    console.log('正在检查更新...');
+  });
+
+  // 监听 'update-available' 事件
+  autoUpdater.on('update-available', (info) => {
+    console.log(`发现新版本 ${info.version}，正在下载...`);
+  });
+
+  // 监听 'update-not-available' 事件
+  autoUpdater.on('update-not-available', () => {
+    console.log('当前已经是最新版本');
+  });
   
+  // 监听 'error' 事件
+  autoUpdater.on('error', (err) => {
+    console.error('自动更新错误', err);
+  });
+
+  autoUpdater.on('download-progress', (progressObj) => {
+    console.log('download-progress')
+  });
+
   //监听'update-downloaded'事件，新版本下载完成时触发
   autoUpdater.on('update-downloaded', () => {
     dialog.showMessageBox({
